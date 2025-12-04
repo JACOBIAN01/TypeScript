011650205564e4b48e04316a2256b6a9071970aa
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-12-04 18:16:28 +0530

    Update

--- learn-typescript/dist/index.js
+++ learn-typescript/dist/index.js
@@ -2,4 +2,24 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let message = "TypeScript";
 console.log(message);
+let age = 25;
+let year = parseInt('2025');
+let decimal = 10.01;
+let hexadecimal = 0xa00d;
+console.log(age, year, decimal, hexadecimal);
+let isEven = true;
+console.log(isEven);
+let data;
+data = "Value";
+data = null;
+let phoneNumber; // Greater than 2^53-1
+// Summery DT -> Primitive Type -> number ,string,boolean,null,undefined,symbol,bigint
+//Type System -> any , unknown , union , custom
+// Declaration -> Annotation -> Inference
+let value;
+value = "String Type";
+value = true;
+value = 10;
+value = null;
+value = undefined;
 //# sourceMappingURL=index.js.map
--- learn-typescript/src/index.ts
+++ learn-typescript/src/index.ts
@@ -1,2 +1,41 @@
-let message:String = "TypeScript"
-console.log(message)
+let message:String = "TypeScript";
+console.log(message);
+
+let age:number = 25;
+let year:number = parseInt('2025');
+let decimal:number = 10.01;
+let hexadecimal:number = 0xa00d;
+console.log(age,year,decimal,hexadecimal)
+
+let isEven:boolean = true;
+console.log(isEven);
+
+let data:string|null;
+
+data = "Value"
+data = null
+
+let phoneNumber :bigint|undefined; // Greater than 2^53-1
+
+// Summery DT -> Primitive Type -> number ,string,boolean,null,undefined,symbol,bigint
+
+//Type System -> any , unknown , union , custom
+// Declaration -> Annotation -> Inference
+
+let value :any;
+value = "String Type"
+value = true
+value = 10
+value = null
+value = undefined
+
+
+
+
+
+
+
+
+
+
+
